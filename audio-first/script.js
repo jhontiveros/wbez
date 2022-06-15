59e899eb67b38a6def448347ebf2131204e791bc
jhontiveros <jennicahontiveros@gmail.com>
2022-06-15 03:06:53 -0500

    CSS additions

--- audio-first/script.js
+++ audio-first/script.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     let wavesurfer = WaveSurfer.create({
         container: document.querySelector('#waveform'),
-        barWidth: 2,
+        waveColor: '#6E6F70',
+        progressColor: '#EB3223',
+        
+        barWidth: 3,
         barHeight: 1, // the height of the wave
         barGap: null // the optional spacing between bars of the wave, if not provided will be calculated in legacy format
     });
     wavesurfer.load('media/frank.mp3');
+
+    const button = document.querySelector('[data-action="play"]');
+    button.addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+
   });
   
