196418f8dbff55d99ef2a7ff3e97552aa9d38818
jhontiveros <jennicahontiveros@gmail.com>
2022-06-15 12:05:38 -0500

    Formatting content

--- audio-first/script.js
+++ audio-first/script.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         container: document.querySelector('#waveform'),
         waveColor: '#6E6F70',
         progressColor: '#EB3223',
+
+        responsive: true,
         
         barWidth: 3,
-        barHeight: 1, // the height of the wave
-        barGap: null // the optional spacing between bars of the wave, if not provided will be calculated in legacy format
+        barHeight: 1,
+        barGap: null
     });
     wavesurfer.load('media/frank.mp3');
 
