029eb32f12fae1467fd8eaecc30855366d2fda69
jhontiveros <jennicahontiveros@gmail.com>
2022-06-21 08:48:01 -0500

    JS additions

--- audio-first/script.js
+++ audio-first/script.js
@@ -37,6 +37,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
         wavesurfer.skip(10);
     }
 
+    let btn = document.querySelector('button');
+    let content = document.querySelector(".detail-content")
 
+    btn.addEventListener('click', () => {
+    if(content.style.display === 'none'){
+        content.style.display = 'block';
+    }else {
+        content.style.display = 'none';
+    }
+
+    })
   });
   
