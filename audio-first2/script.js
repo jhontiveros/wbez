a431c0087f39f161e359a73fb896ac411fe355e7
jhontiveros <jennicahontiveros@gmail.com>
2022-06-22 03:59:04 -0500

    Adding content

--- audio-first2/script.js
+++ audio-first2/script.js
@@ -2,7 +2,7 @@ let btn = document.querySelector('button');
 let content = document.querySelector(".detail-content")
 
 btn.addEventListener('click', () => {
-    if(content.style.display === 'none'){
+    if(content.style.display !== 'block'){
         content.style.display = 'block';
     }else {
         content.style.display = 'none';
